b616d255a54dbbeed442c1e091e74bd84c4d95bf
cs.msu.su <cs.msu.su@7e25931e-fce2-11dd-87c9-111d11f6bf44>
2009-02-23 18:13:01 +0000

    [~] edit: added SSE code

--- Common/HelloWorld.cpp
+++ Common/HelloWorld.cpp
@@ -2,20 +2,23 @@
 
 #include "HelloWorld.h"
 
-float ST_SimpleAddKernel( float * pA, float * pB, float * pC, int n)
+float ST_SimpleAddKernel( float * pA, float * pB, float * pC, int n, int times)
 {
     clock_t start, stop;
 
     start = clock();
 
-    for (int idx = 0; idx < n; idx++)
+    for (int itimes = 0; itimes < times; itimes++)
     {
-        pC[idx] = pA[idx] + pB[idx];
+        for (int idx = 0; idx < n; idx++)
+        {
+            pC[idx] = pA[idx] + pB[idx];
+        }
     }
 
     stop = clock();
 
-    float cpuTime = (stop - start)  * 1000.0f / CLOCKS_PER_SEC;
+    float cpuTime = (stop - start) * 1000.0f / CLOCKS_PER_SEC;
 
     return cpuTime;
 }
@@ -33,15 +36,23 @@ void MT_SimpleAddKernel(void * pVoid)
     }
 }
 
-void MT_SimpleAddKernel2(void * pVoid)
+float SSE_SimpleAddKernel( __m128 * pA, __m128 * pB, __m128 * pC, int n, int times)
 {
-    //SArgList *pArgList = (SArgList *) pVoid;
+    clock_t start, stop;
 
-    //for (int itimes = 0; itimes < pArgList->times; itimes++)
-    //{
-    //    for (int idx = pArgList->start ; idx < pArgList->stop; idx++)
-    //    {
-    //        pArgList->pC[idx] = pArgList->pA[idx] + pArgList->pB[idx];
-    //    }
-    //}
+    start = clock();    
+
+    for (int itimes = 0; itimes < times; itimes++)
+    {
+        for (int idx = 0; idx < n; idx++)
+        {
+            pC[idx] = _mm_add_ps( pA[idx], pB[idx] );        
+        }
+    }
+
+    stop = clock();
+
+    float sseTime = (stop - start) * 1000.0f / CLOCKS_PER_SEC;
+
+    return sseTime;
 }
--- Common/HelloWorld.h
+++ Common/HelloWorld.h
@@ -1,6 +1,9 @@
 #ifndef _HELLO_WORLD_H_
 #define _HELLO_WORLD_H_
 
+
+#include <xmmintrin.h>
+
 struct SArgList
 {
     float * pA;
@@ -12,8 +15,8 @@ struct SArgList
 };
 
 void  MT_SimpleAddKernel( void * pVoid);
-void  MT_SimpleAddKernel2(void * pVoid);
-float ST_SimpleAddKernel( float * pA, float * pB, float * pC, int n);
+float SSE_SimpleAddKernel( __m128 * pA, __m128 * pB, __m128 * pC, int n, int times = 100);
+float ST_SimpleAddKernel( float * pA, float * pB, float * pC, int n, int times = 100);
 float CU_SimpleAddKernel( float * pA, float * pB, float * pC, int * threads, int * blocks, int n);
 
 #endif
