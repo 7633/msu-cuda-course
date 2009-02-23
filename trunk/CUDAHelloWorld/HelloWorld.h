8ea5c55dea033570357a62718b3ef60bb0379683
cs.msu.su <cs.msu.su@7e25931e-fce2-11dd-87c9-111d11f6bf44>
2009-02-23 13:26:31 +0000

    [-] removing from CUDAHelloWorld project, moving to separate [Common] folder

--- trunk/CUDAHelloWorld/HelloWorld.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HELLO_WORLD_H_
-#define _HELLO_WORLD_H_
-
-float ST_SimpleAddKernel( float * pA, float * pB, float * pC, int n);
-float CU_SimpleAddKernel( float * pA, float * pB, float * pC, int * threads, int * blocks, int n);
-
-#endif
